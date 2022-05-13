06df908a884112af85f85d97b138afe0d7e3031b
Chirag Jhumkhawala <chirag912@gmail.com>
2022-05-13 09:31:17 +0530

    header change

--- src/index.js
+++ src/index.js
@@ -152,8 +152,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Real-Time Object Detection: Kangaroo</h1>
-        <h3>MobileNetV2</h3>
+        <h1>Real-Time Object Detection: HDFC</h1>
+        <h3>By:Dataseed Tech</h3>
         <video
           style={{height: '600px', width: "500px"}}
           className="size"
